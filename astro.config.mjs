48f89dce945a310c9ed6622b3f5daf5c0e12e002
Valentin Marquez <valentin13.mail@gmail.com>
2025-04-27 10:06:50 +0000

    style: standardize quotation marks and formatting in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,23 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
-import cloudflare from '@astrojs/cloudflare';
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  },
-  integrations: [react()],
-  output: 'server',
-  adapter: cloudflare()
+	vite: {
+		plugins: [tailwindcss()],
+		resolve: {
+			alias: import.meta.env.PROD && {
+				"react-dom/server": "react-dom/server.edge",
+			},
+		},
+	},
+	integrations: [react()],
+	output: "server",
+	adapter: cloudflare(),
 });
